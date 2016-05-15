41a7357c8ea4ed1a2b816635f40a53851ebddf55
David Young-chan Kay <davidykay@gmail.com>
2016-05-14 23:33:59 -0700

    menu drawer looking good. next up: arrow button

--- src/cljs/pharmacy/panels/menu_drawer.cljs
+++ src/cljs/pharmacy/panels/menu_drawer.cljs
@@ -1,6 +1,9 @@
-(ns pharmacy.panels.menu-drawer)
+(ns pharmacy.panels.menu-drawer
+  (:require 
+   [pharmacy.components.searchbox :refer [searchbox]]
+   ))
 
-(def items
+(def debug-items
   [["Home" "#/"]
    ["Drug Detail" "#/drug/1234"]
    ["Dashboard" "#/dashboard"]
@@ -8,14 +11,46 @@
    ["Past Orders" "#/orders"]
    ["Join" "#/join"]])
 
+(def items
+  [["My Dashboard" "#/dashboard"]
+   ["My Prescription" "#/prescription"]
+   ["Checkout (2)" "#/checkout"]])
+
+(def recent-drugs
+  [["Atorvastatin" "#/drug/1234"]
+   ["Drug B" "#/drug/5678"]
+   ["Drug C" "#/drug/9012"]])
+
+(defn link-item [n p]
+  [:h2.subtitle [:a {:href p} n]])
+
 (defn component []
   (fn []
     [:div.columns
      [:div.column.container
+
       [:section.section
        [:div.container
         [:h1.title "Menu Drawer"]]]
+
+      [:section.section
+       [:div.container
+        [searchbox {:placeholder "Find Medication"
+                    :on-search #(print "searching for" %)}]]]
+      
       [:section.section
        [:div.container
         (for [[n p] items]
-          [:h2.subtitle [:a {:href p} n]])]]]]))
+          [:h1.title [:a {:href p} n]])]]
+
+      [:section.section
+       [:div.container
+        [:h2.subtitle "Recently Browsed"]
+        (for [[n p] recent-drugs]
+          [link-item n p])]]
+
+      [:section.section
+       [:div.container
+        [:h2.subtitle "Debug"]
+        (for [[n p] debug-items]
+          [link-item n p])]]]]))

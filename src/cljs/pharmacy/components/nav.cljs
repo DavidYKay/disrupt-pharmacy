131a9ffa51c2013a652dfe60e41424448ace2198
David Young-chan Kay <davidykay@gmail.com>
2016-05-14 17:12:53 -0700

    huge nav kluge

--- src/cljs/pharmacy/components/nav.cljs
+++ src/cljs/pharmacy/components/nav.cljs
@@ -1,20 +1,23 @@
 (ns pharmacy.components.nav
   (:require
-   [pharmacy.routes :refer [home about]]
+   ;;[pharmacy.routes :refer [home about]]
    [clojure.walk :refer [postwalk]]
    [re-frame.core :as re-frame :refer [subscribe]]))
 
 (def items
-  [["Home" home]
-   ["About" about]
-   ["Terms" about]
-   ["Service" about]
-   ["Consultation" about]
+  [
+   ["Home" "#"]
+   ["About" "#/about"]
+   ;;["Home" (home)]
+   ;;["About" (about)]
+   ;;["Terms" about]
+   ;;["Service" about]
+   ;;["Consultation" about]
    ])
 
 (defn component []
   (fn []
     [:div "Navigation"
      [:ul
-      (for [[n f] items]
-        [:li [:a {:href (f)} n]])]]))
+      (for [[n p] items]
+        [:li [:a {:href p} n]])]]))

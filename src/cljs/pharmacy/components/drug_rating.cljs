0fc57da29f865dcf77af0c2c9ffbe3644b716439
bdotq <qbrian+github@gmail.com>
2016-05-20 03:54:30 -0700

    Recoded the drug score widget - NOW SCALEABLE!
    Originally coded with bulma .tiles, but bulma tiles do not keep layout
    in mobile view, they go full width. Recoded with columns class and
    custom css.
    The widget is coded in em to be scaleable! To use, set the font-size of
    class .score-box to desired size. [eg. .drugbible-page .score-box
    {font-size:60pt}]

--- src/cljs/pharmacy/components/drug_rating.cljs
+++ src/cljs/pharmacy/components/drug_rating.cljs
@@ -2,18 +2,18 @@
   (:require [re-frame.core :as re-frame :refer [dispatch]]))
 
 (defn drug-rating []
-  [:div.tile.is-ancestor
-   [:div.tile.is-parent
-    [:div.tile.is-child.box
-     [:div "B"]
-     [:div "Overall Score"]
+  [:div.score-box.is-inline-flex.columns.is-gapless.has-text-centered
+   [:div.column.is-two-third
+    [:div.scorebox-overall
+     [:div.grade "B"]
+     [:div.grade-label "Overall Score"]
      ]]
-   [:div.tile.is-parent.is-vertical
-    [:div.tile.is-child.box
-     [:div "A"]
-     [:div "Effectiveness"]
+   [:div.column.is-one-third
+    [:div.scorebox-efficacy
+     [:div.grade "A"]
+     [:div.grade-label "Effectiveness"]
      ]
-    [:div.tile.is-child.box
-     [:div "C"]
-     [:div "Side Effects"]
+    [:div.scorebox-sidefx
+     [:div.grade "C"]
+     [:div.grade-label "Side Effects"]
      ]]])

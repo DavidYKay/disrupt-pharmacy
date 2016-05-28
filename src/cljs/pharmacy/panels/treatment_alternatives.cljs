926f5188062bc58ff4ef8baa70dbaed4492e3446
David Young-chan Kay <davidykay@gmail.com>
2016-05-28 16:50:43 -0700

    hipster therapy now links to respective drug pages.

--- src/cljs/pharmacy/panels/treatment_alternatives.cljs
+++ src/cljs/pharmacy/panels/treatment_alternatives.cljs
@@ -1,5 +1,6 @@
 (ns pharmacy.panels.treatment-alternatives
   (:require
+   [clojure.string :refer [lower-case]]
    [pharmacy.components.drug-rating :refer [drug-rating]]
    [pharmacy.components.top-bar :refer [top-bar]]))
 
@@ -8,15 +9,14 @@
                       {:name "Diet" :rating {:current 2 :max 8} :description "hello world" }
                       {:name "Atorvastatin" :rating {:current 3 :max 8} :description "hello world" }
                       {:name "Rosuvastatin" :rating {:current 3 :max 8} :description "hello world" }
-                      {:name "Simvastatin" :rating {:current 4 :max 5} :description "hello world" }
+                      {:name "Simvastatin" :rating {:current 4 :max 8} :description "hello world" }
+                      {:name "Pravastatin" :rating {:current 5 :max 8} :description "hello world" }
                       {:name "Lovastatin" :rating {:current 6 :max 8} :description "hello world" }
                       {:name "Fluvastatin" :rating {:current 6 :max 8} :description "hello world" }]]
 
     ;; Atorvastatin -> Rasuvastatin
     ;; Consult Rasuvastatin
     ;; Adaptation conversation with dr.
-    ;; Followup email
-    ;; Survey email
 
     (fn []
       [:div
@@ -27,6 +27,8 @@
        (for [{:keys [rating name description]} alternatives]
          ^{:key name}
          [:div.box
-          [drug-rating (:current rating)]
-          [:h2.subtitle name]
-          [:div description]])])))
+          [:a {:href (str "/#/drug/" (lower-case name))}
+           ;; [drug-rating (:current rating)]
+           [:div (:current rating) "/" (:max rating)]
+           [:h2.subtitle name]
+           [:div description]]])])))

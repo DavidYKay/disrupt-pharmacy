56d63c6ff21edd7c6dd3c845c1ee730a9ec727b5
David Young-chan Kay <davidykay@gmail.com>
2016-06-05 17:43:55 -0700

    rough pass at the new followup email

--- src/cljs/pharmacy/panels/follow_up_email.cljs
+++ src/cljs/pharmacy/panels/follow_up_email.cljs
@@ -8,7 +8,22 @@
     [:div.box.container
      [:h1.title "Follow-up Email"]
 
-     [:p "You have been taking Atorvastatin for 1 month!  How has it been?  Let us know how your treatment is going by logging into your DrugIQ account and completing a short follow-up questionnaire, and provide any additional feedback you may have about your experience with the medication, as well as our service overall.  This will help us to improve our research, and ultimately provide you and the rest of our patients with the most optimal care."]
+     [:p "Hi Mark!"]
+     [:p "This is your drugiQ check up."]
+     [:p "You’ve been taking atorvastatin for a month and we want to know how it’s been going."]
+     [:p "Let us know the good or the bad with atorvastatin. What you share will help us learn what will work for you now and in the future, and also help the rest of the drugiQ community."]
+     [:p "Please fill out the box below with anything you’ve discovered after taking atorvastatin:"]
+
+     [:input.input.is-large
+      {:placeholder "It's been great! / I've experienced some side effects..."
+       :type "text"
+       ;:on-key-down (on-enter save)
+       ;;:on-change (fn [ev]
+       ;;             (reset! val (-> ev .-target .-value)))
+       }]
+
+     [:a.button "Submit"]
+ 
 
      [:p "Thank you!"]
 

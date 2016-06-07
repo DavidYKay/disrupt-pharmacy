6de97eb52c42c4133f9dd08b8bd8604dd71db2f5
bdotq <qbrian+github@gmail.com>
2016-06-07 03:20:55 -0700

    removed X button from modals
    because the X button doesnt perform any action at this point.

--- src/cljs/pharmacy/components/utils/modal.cljs
+++ src/cljs/pharmacy/components/utils/modal.cljs
@@ -1,5 +1,5 @@
 (ns pharmacy.components.utils.modal)
-  
+
 (defn make-modal [modal-shown on-dismiss title content button]
   [:div {:class (if modal-shown
                   "modal is-active"
@@ -7,10 +7,8 @@
    [:div.modal-background {:on-click on-dismiss}]
    [:div.modal-card
     [:header.modal-card-head
-     [:p.modal-card-title title]
-     [:button.delete]]
+     [:p.modal-card-title title]]
     [:section.modal-card-body content]
     [:footer.modal-card-foot
      [:a.button.is-primary {:href (:href button)
                             :on-click (:on-click button)} (:text button)]]]])
-    

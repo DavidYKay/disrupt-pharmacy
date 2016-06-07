a988f8360aa7bd9b5aa43738cc2093284ee9496d
David Young-chan Kay <davidykay@gmail.com>
2016-06-07 00:19:02 -0700

    corrected the copy on the drug scores modal.

--- src/cljs/pharmacy/components/drug_scores_modal.cljs
+++ src/cljs/pharmacy/components/drug_scores_modal.cljs
@@ -9,6 +9,9 @@
                 dismiss
                 "Drug Scores"
                 [:div
-                 [:p "Drug scores are collected from over 9000 pharmacists"]]
+                 [:p "The " [:strong " percent score "] " indicates how many pharmacists would take this drug, given a risk score of A, B, C, or D."]
+
+                 [:br]
+                 [:p "The " [:strong " risk score "] " indicates the risk of experiencing a cardiovascular event (stroke, heart attack, death), where A is a low risk and D is a high risk."]]
                 {:on-click dismiss
                  :text "OK"})))

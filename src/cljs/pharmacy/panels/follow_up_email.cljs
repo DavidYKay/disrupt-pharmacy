4346cec698881914782001544eca031ec98aaaec
David Young-chan Kay <davidykay@gmail.com>
2016-06-06 21:48:06 -0700

    decapitalized Atorvastatin.

--- src/cljs/pharmacy/panels/follow_up_email.cljs
+++ src/cljs/pharmacy/panels/follow_up_email.cljs
@@ -10,10 +10,10 @@
 
      [:p "Hi Mark!"]
      [:br]
-     [:p "You’ve been taking Atorvastatin for a month and we want to know how it’s been going."]
+     [:p "You’ve been taking atorvastatin for a month and we want to know how it’s been going."]
      [:br]
      
-     [:p "Let us know the good or the bad with Atorvastatin. What you share will help us learn what will work for you now and in the future, and also help the rest of the DrugIQ community."]
+     [:p "Let us know the good or the bad with atorvastatin. What you share will help us learn what will work for you now and in the future, and also help the rest of the DrugIQ community."]
      [:br]
      [:p "Please fill out the box below with anything you’ve discovered after taking atorvastatin:"]
 
